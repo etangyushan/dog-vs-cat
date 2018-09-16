14bcef44a94cd2855da3a33bf926ac085b3ce893
Ubuntu <ubuntu@ip-172-31-15-42.ap-northeast-1.compute.internal>
2018-09-16 03:57:37 +0000

    fix(install)

--- install/enveriment_config.sh
+++ install/enveriment_config.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# 获取项目源码
+echo "0.获取项目源码"
+#git clone https://github.com/etangyushan/dog-vs-cat.git
+
 # 安装 anaconda
 echo "1.安装 anaconda"
 wget http://repo.continuum.io/archive/Anaconda3-4.3.0-Linux-x86_64.sh
@@ -10,11 +14,10 @@ source ~/.bashrc
 getchar()
 
 
-# 获取项目源码
-echo "2.获取项目源码"
 
-git clone https://github.com/etangyushan/dog-vs-cat.git
-conda env create -f dog-vs-cat/requirements/dog-linux.yml
+# 运行环境配置 
+echo "2.运行环境配置"
+conda env create -f ../requirements/dog-linux.yml
 
 getchar()
 
@@ -24,7 +27,7 @@ echo "3.安装 kaggle"
 pip install --user kaggle
 pip install kaggle-cli
 mkdir ~/.kaggle
-cp dog-vs-cat/requirements/kaggle.json ~/.kaggle
+cp ../requirements/kaggle.json ~/.kaggle
 chmod 600 ~/.kaggle/kaggle.json
 kaggle competitions download -c dogs-vs-cats-redux-kernels-edition
 
@@ -36,7 +39,7 @@ echo "4.安装 unzip 解压数据集"
 sudo apt install unzip
 unzip test.zip
 unzip train.zip
-mv test train dog-vs-cat/images
+mv test train ../images
 
 getchar()
 
@@ -46,6 +49,8 @@ echo "5.配置 jupyter notebook"
 source activate dog-project
 jupyter notebook --generate-config
 jupyter notebook password
+cp jupyter_notebook_config.py  ~/.jupyter/jupyter_notebook_config.py
+
 echo "
 vim ~/.jupyter/jupyter_notebook_config.py
 c.NotebookApp.ip='*'
@@ -57,6 +62,7 @@ getchar()
 
 # 启动项目后台运行
 echo "6.启动项目后台运行"
+cd 
 nohup jupyter notebook &
 
 
